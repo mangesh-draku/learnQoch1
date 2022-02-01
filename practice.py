64c4f012ae4bb3bb8107b56093a17a433ea21ed3
mangesh-draku <98479010+mangesh-draku@users.noreply.github.com>
2022-02-01 15:51:29 +0530

    Initial commit

---
+++ practice.py
@@ -0,0 +1,6 @@
+# x,y=input("enter value for x and y with space seperation = ").split()
+# print("x = ",x)
+# print("y = ",y)
+
+x = list(map(int, input("enter multiple value : ").split()))
+print("list of students : ",x)
