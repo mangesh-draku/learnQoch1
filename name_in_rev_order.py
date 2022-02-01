c932f8e7ee3f75dc126ea32ebe97621b22da4165
mangesh-draku <98479010+mangesh-draku@users.noreply.github.com>
2022-02-01 17:02:08 +0530

    added new file

---
+++ name_in_rev_order.py
@@ -0,0 +1,3 @@
+users_fname=input("Enter First name : ")
+users_Lfname=input("Enter Last name : ")
+print(users_fname[::-1]+" "+users_Lfname[::-1])
--- practice.py
+++
@@ -1,6 +0,0 @@
-# x,y=input("enter value for x and y with space seperation = ").split()
-# print("x = ",x)
-# print("y = ",y)
-
-x = list(map(int, input("enter multiple value : ").split()))
-print("list of students : ",x)
